e298ab28534b10ecf8805fc2b0451e84a1d71438
Александр Канов <monersen@gmail.com>
2023-04-30 09:31:51 +0300

    ООП: наследование и полиморфизм

--- 5.3/5.3.cpp
+++ 5.3/5.3.cpp
@@ -1,272 +1,222 @@
 ﻿#include <iostream>
-#include <string>
-#include <Windows.h>
- 
 
 using namespace std;
 
-
-class Figure
-{
-protected:
-    string name = "Фигура"; 
-    int num_of_sides = 0;
-
+class Figure {
 public:
-    string get_name() { return name; };
-    int get_sides() { return num_of_sides; }
-    virtual bool check() { return true; }
-    virtual void print_info() {
-        cout << get_name() << ":" << endl;
-        if (check()) { cout << "Правильная" << endl; }
-        else { cout << "Неправильная" << endl; }
-        cout << "Количество сторон: " << get_sides() << endl;
-    };
+    Figure() : Figure(0, "Фигура") {}
 
+    int getSidesCount() {
+        return sides_count;
+    }
+
+    virtual void printInfo() {
+
+        cout << endl << name << ":" << endl;
+
+        if (check())
+            cout << "Правильная" << endl;
+        else
+            cout << "Неправильная" << endl;
+
+        cout << "Колличество сторон: " << sides_count << endl;
+    }
+
+protected:
+    int sides_count;
+    string name;
+
+    Figure(int s, string _name)
+        : sides_count(s)
+        , name(_name)
+    {}
+
+    virtual bool check() {
+        return sides_count == 0 ? true : false;
+    }
 };
 
-class Triangle : public Figure
-{
+class Triangle : public Figure {
+
 public:
-    Triangle() { a = b = c = 0; A = B = C = 0; }
-    Triangle(int a_, int b_, int c_, int A_, int B_, int C_) 
-    {
-        a = a_;
-        b = b_;
-        c = c_;
-        A = A_;
-        B = B_;
-        C = C_;
-        name = "Треугольник";
-        num_of_sides = 3;
-    }
-   
-    bool check() override {
-        if (A + B + C == 180) { return true; }
-        else { return false; }
-    }
-    
-    void print_info() override {
-        Figure::print_info();
-        cout << "Стороны: a=" << get_a() << " b=" << get_b() << " c=" << get_c() << endl;
-        cout << "Углы: A=" << get_A() << " B=" << get_B() << " C=" << get_C() << endl;
+    Triangle(int a, int b, int c, int A, int B, int C)
+        : Figure(3, "Треугольник")
+        , sides{ a, b, c }
+        , angles{ A, B, C }
+    {}
+
+    void printInfo() override {
+        Figure::printInfo();
+
+        cout << "Стороны:";
+        getSides();
+
+        cout << "\nУглы:";
+        getAngles();
+
         cout << endl;
     }
-   
-    int get_a() { return a; };
-    int get_b() { return b; };
-    int get_c() { return c; };
-    int get_A() { return A; };
-    int get_B() { return B; };
-    int get_C() { return C; };
+
+    void getSides() {
+        int i = 0;
+        for (char c = 'a'; c <= 'c'; ++c && ++i)
+            cout << c << "=" << sides[i] << " ";
+    }
+
+    void getAngles() {
+        int i = 0;
+        for (char c = 'A'; c <= 'C'; ++c && ++i)
+            cout << c << "=" << angles[i] << " ";
+    }
 
 protected:
-    int a, b, c;
-    int A, B, C;
+    int sides[3];
+    int angles[3];
+
+    virtual bool check() override {
+        int angles_sum = 0;
+
+        for (int i = 0; i < sides_count; ++i)
+            angles_sum += angles[i];
+
+        return sides_count == 3 && angles_sum == 180 ? true : false;
+    }
 };
 
-class RightTriangle : public Triangle
-{
+class RightTriangle : public Triangle {
 public:
-    RightTriangle(int a_, int b_, int c_, int A_, int B_)
-    {
-        a = a_;
-        b = b_;
-        c = c_;
-        A = A_;
-        B = B_;
-        C = 90;
+    RightTriangle(int a, int b, int c, int A, int B) : Triangle(a, b, c, A, B, 90) {
         name = "Прямоугольный треугольник";
-        num_of_sides = 3;
     }
-    
-    bool check() override {
-        if (Triangle::check() && C == 90) { return true; }
-        else { return false; }
-    }
-
 };
 
-class IsoscelesTriangle : public Triangle
-{
+class IsosTriangle : public Triangle {
 public:
-    IsoscelesTriangle(int size_ac, int b_, int size_AC, int B_)
-    {
-        a = c = size_ac;
-        b = b_;
-        A = C = size_AC;
-        B = B_;
+    IsosTriangle(int a, int b, int A, int B) : Triangle(a, b, a, A, B, A) {
         name = "Равнобедренный треугольник";
-        num_of_sides = 3;
     }
-    
-    bool check() override {
-        if (Triangle::check() && a == b && A == C) { return true; }
-        else { return false; }
-    }
-
 };
 
-class EquilateralTriangle : public Triangle
-{
+class EquilTriangle : public Triangle {
 public:
-    EquilateralTriangle(int sizes)
-    {
-        a = b = c = sizes;
-        A = B = C = 60;
+    EquilTriangle(int a) : Triangle(a, a, a, 60, 60, 60) {
         name = "Равносторонний треугольник";
-        num_of_sides = 3;
     }
-    
-    bool check() override {
-        if (Triangle::check() && a == b && a == c && b == c && A == 60 && B == 60 && C == 60) { return true; }
-        else { return false; }
-    }
-};
-
-class Quadrangle : public Figure
-{
-public:
-    Quadrangle() { a = b = c = d = 0; A = B = C = D = 0; }
-    Quadrangle(int a_, int b_, int c_, int d_, int A_, int B_, int C_, int D_) 
-    {
-        a = a_;
-        b = b_;
-        c = c_;
-        d = d_;
-        A = A_;
-        B = B_;
-        C = C_;
-        D = D_;
-        name = "Четырехугольник";
-        num_of_sides = 4;
-    }
-    
-    bool check() override {
-        if (A + B + C + D == 360) { return true; }
-        else { return false; }
-    }
-    
-    void print_info() override {
-        Figure::print_info();
-        cout << "Стороны: a=" << get_a() << " b=" << get_b() << " c=" << get_c() << " d=" << get_d() << endl;
-        cout << "Углы: A=" << get_A() << " B=" << get_B() << " C=" << get_C() << " D=" << get_D() << endl;
-        cout << endl;
-    }
-    
-    int get_a() { return a; };
-    int get_b() { return b; };
-    int get_c() { return c; };
-    int get_d() { return d; };
-    int get_A() { return A; };
-    int get_B() { return B; };
-    int get_C() { return C; };
-    int get_D() { return D; };
 
 protected:
-    int a, b, c, d;
-    int A, B, C, D;
+    virtual bool check() {
+        return true;
+    }
 };
 
-class RectangleF : public Quadrangle
-{
+class Quadrilateral : public Figure {
+
 public:
-    RectangleF(int a_, int b_) {
-        a = c = a_;
-        b = d = b_;
-        A = B = C = D = 90;
+    Quadrilateral(int a, int b, int c, int d, int A, int B, int C, int D)
+        : Figure(4, "Четырёхугольник")
+        , sides{ a, b, c, d }
+        , angles{ A, B, C, D }
+    {}
+
+    void printInfo() override {
+        Figure::printInfo();
+
+        cout << "Стороны:";
+        getSides();
+
+        cout << "\nУглы:";
+        getAngles();
+
+        cout << endl;
+    }
+
+    void getSides() {
+        int i = 0;
+        for (char c = 'a'; c <= 'd'; ++c && ++i)
+            cout << c << "=" << sides[i] << " ";
+    }
+
+    void getAngles() {
+        int i = 0;
+        for (char c = 'A'; c <= 'D'; ++c && ++i)
+            cout << c << "=" << angles[i] << " ";
+    }
+
+protected:
+    int sides[4];
+    int angles[4];
+
+    virtual bool check() override {
+        int angles_sum = 0;
+        for (int i = 0; i < sides_count; ++i)
+            angles_sum += angles[i];
+
+        return sides_count == 4 && angles_sum == 360 ? true : false;
+    }
+};
+
+class Rectangle : public Quadrilateral {
+public:
+    Rectangle(int a, int b) : Quadrilateral(a, b, a, b, 90, 90, 90, 90) {
         name = "Прямоугольник";
-        num_of_sides = 4;
-    }
-   
-    bool check() override {
-        if (Quadrangle::check() && a == c && b == d && A == 90 && B == 90 && C == 90 && D == 90) { return true; }
-        else { return false; }
     }
 };
 
-class Square : public Quadrangle
-{
+class Square : public Quadrilateral {
 public:
-    Square(int a_) {
-        a = c = b = d = a_;
-        A = B = C = D = 90;
+    Square(int a) : Quadrilateral(a, a, a, a, 90, 90, 90, 90) {
         name = "Квадрат";
-        num_of_sides = 4;
-    }
-  
-    bool check() override {
-        if (Quadrangle::check() && a == c && b == d && a == b && A == 90 && B == 90 && C == 90 && D == 90) { return true; }
-        else { return false; }
     }
 };
 
-class Parallelogram : public Quadrangle
-{
+class Parallelogram : public Quadrilateral {
 public:
-    Parallelogram(int a_, int b_, int A_, int B_) {
-        a = c = a_;
-        b = d = b_;
-        A = C = A_;
-        B = D = B_;
+    Parallelogram(int a, int b, int A, int B) : Quadrilateral(a, b, a, b, A, B, A, B) {
         name = "Параллелограмм";
-        num_of_sides = 4;
-    }
-   
-    bool check() override {
-        if (Quadrangle::check() && a == c && b == d && A == C && B == D) { return true; }
-        else { return false; }
     }
 };
 
-class Rhomb : public Quadrangle
-{
+class Rhombus : public Quadrilateral {
 public:
-    Rhomb(int a_, int A_, int B_) {
-        a = c = b = d = a_;
-        A = C = A_;
-        B = D = B_;
+    Rhombus(int a, int A, int B) : Quadrilateral(a, a, a, a, A, B, A, B) {
         name = "Ромб";
-        num_of_sides = 4;
-    }
-  
-    bool check() override {
-        if (Quadrangle::check() && a == c && b == d && a == b && A == C && B == D) { return true; }
-        else { return false; }
     }
 };
 
-
+void printInfo(Figure* base) {
+    base->printInfo();
+}
 
 int main(int argc, char** argv)
 {
+    setlocale(LC_ALL, "Russian");
 
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-  //  setlocale(LC_ALL, "Russian");
+    Figure fig;
+    printInfo(&fig);
 
-  
-    Figure f;
-    f.print_info();
-    cout << endl;   
-    Triangle t(10, 20, 30, 50, 60, 70);
-    t.print_info();  
-    RightTriangle rt(10, 20, 30, 50, 60);
-    rt.print_info();
-    IsoscelesTriangle i(10, 20, 50, 60);
-    i.print_info();
-    EquilateralTriangle e(30);
-    e.print_info();
-    Quadrangle q(10, 20, 30, 40, 50, 60, 70, 80);
-    q.print_info();
-    RectangleF rf(10, 20);
-    rf.print_info();
-    Square s(20);
-    s.print_info();
-    Parallelogram p(20, 30, 30, 40);
-    p.print_info();
-    Rhomb romb(30, 30, 40);
-    romb.print_info();
+    Triangle tri(10, 20, 30, 50, 60, 70);
+    RightTriangle riTri1(10, 20, 30, 50, 60);
+    RightTriangle riTri2(10, 20, 30, 50, 40);
+    IsosTriangle isosTri(10, 20, 50, 60);
+    EquilTriangle equTri(30);
+
+    printInfo(&tri);
+    printInfo(&riTri1);
+    printInfo(&riTri2);
+    printInfo(&isosTri);
+    printInfo(&equTri);
+
+    Quadrilateral qua(10, 20, 30, 40, 50, 60, 70, 80);
+    Rectangle rec(10, 20);
+    Square squ(20);
+    Parallelogram par(20, 30, 30, 40);
+    Rhombus rom(30, 30, 40);
+
+    printInfo(&qua);
+    printInfo(&rec);
+    printInfo(&squ);
+    printInfo(&par);
+    printInfo(&rom);
 
     return 0;
 }
